7d288a6120a96448c1311934d02e881e64c6ce4f
FunBinge <cardenas.eduardo10@yahoo.com.au>
2016-02-05 14:25:39 +1030

    Cleaning up old scripts

---
+++ Elemental REV/Assets/Editor/SmartItweenTriggerEditor.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// Custom Inspector YEAH ohh YEAH!!
+/// </summary>
+[CustomEditor(typeof(SmartItweenObject)), CanEditMultipleObjects]
+[System.Serializable]
+public class SmartItweenTriggerEditor : Editor
+{
+
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        var myScript = target as SmartItweenObject;
+
+        if (myScript.triggerType == SmartItweenObject.TriggerType.activatedOnce)
+        {
+            //Reset to origin options
+            myScript.resetToOriginalPos = EditorGUILayout.Toggle("Reset to Origin", myScript.resetToOriginalPos);
+            if (myScript.resetToOriginalPos)
+            {
+                myScript.resetSpeed = EditorGUILayout.Slider("Return Speed", myScript.resetSpeed, 0, 50);
+                myScript.originalPosOffset = EditorGUILayout.Vector3Field("Original Position Offset", myScript.originalPosOffset);
+                myScript.toOriginEaseType = (SmartItweenObject.EaseType)EditorGUILayout.EnumPopup("Return EaseType", myScript.toOriginEaseType);
+            }
+
+            //Unity Event
+            myScript.callEventOnFinish = EditorGUILayout.Toggle("CallEventOnFinish", myScript.callEventOnFinish);
+            if (myScript.callEventOnFinish)
+            {
+                SerializedProperty OnPlayOnce = serializedObject.FindProperty("OnPlayOnceFinished");
+                EditorGUIUtility.LookLikeControls();
+                EditorGUILayout.PropertyField(OnPlayOnce);
+            }
+        }
+
+        //So that settings stay on play (in case of prefabs)
+        if (GUI.changed)
+        {
+            EditorUtility.SetDirty(target);
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
--- Elemental REV/Assets/Scripts/HeroMove.cs
+++ Elemental REV/Assets/Scripts/HeroMove.cs
@@ -5,79 +5,69 @@ using InControl;
 
 public class HeroMove : MonoBehaviour {
 
-    public enum characters { None, Earth, Water, Fire, Air };
-    public characters character;
-
     public LayerMask whatIsGround;
-    public LayerMask noJumpingLayers;   // Layers that are detected as ground yet we dont want the player to jump on (i.e. Water and Lava in the case of the fire and water elementals).
 
     // public modifier variables
     public float moveForceModifier = 3f;
-    public float jumpHeight = 5f;
     public float maxVelocityChange = 10;
     public float gravity = 10.0f;
     public float rotateSpeed = 5;
-    public bool enableFallDamage;
 
+    private Animator _animationControl;
+    private Rigidbody _rgBody;
+    private CapsuleCollider _capsCollider;           //To use the centre of the collider to determine groundcheckOffset instead of the objects's centre
 
-    private Animator animationControl;
-    private Rigidbody rgBody;
-    private CapsuleCollider capsCollider;           //To use the centre of the collider to determine groundcheckOffset instead of the objects's centre
+    private bool _canMove = true;
+    private const float GroundedRadius = 0.2f;
+    private float _groundedOffsetDistance;
+    private Vector3 _movingGroundVelocity = Vector3.zero;
+    private bool _isGrounded;
+    private const string MOVING_GROUND_LAYER_NAME = "MovingGround";
 
-    private bool canUseAbility = true;              //Ed changes: To stop the character from using their special ability when the camera is not looking at them
-    private bool canMove = true;
-    private const float groundedRadius = 0.2f;
-    private float groundedOffsetDistance;
-    private Vector3 movingGroundVelocity = Vector3.zero;
-    private bool isGrounded;
-    private bool manualGravity;                     //use manual gravity
-
-    
-    public bool IsGrounded { get { return isGrounded; } private set { isGrounded = value; } }
-
-    public bool ManualGravity { get { return manualGravity; } set { manualGravity = value; } }
-    public bool CanMove { get { return canMove; } set { canMove = value; } }
-    public bool CanUseAbility { get { return canUseAbility; } set { canUseAbility = value; } }
-    public bool IsMoving { get { return rgBody.velocity != Vector3.zero; } }
+    private Vector3 _groundDetectPos;
 
     void Start()
     {
         // initializing variables
-        capsCollider = GetComponent<CapsuleCollider>();
-        groundedOffsetDistance = -capsCollider.bounds.extents.y + (groundedRadius * 0.7f);
-        animationControl = GetComponent<Animator>();
-        rgBody = GetComponent<Rigidbody>();
+        _capsCollider = GetComponent<CapsuleCollider>();
+        _groundedOffsetDistance = -_capsCollider.bounds.extents.y;
+        _animationControl = GetComponent<Animator>();
+        _rgBody = GetComponent<Rigidbody>();
 
     }
+
 
     void LateUpdate()
     {
        
-        if (!canMove && isGrounded && (rgBody.velocity != Vector3.zero))
+        if (!_canMove && _isGrounded && (_rgBody.velocity != Vector3.zero))
         {
-            Vector3 vel = Vector3.Lerp(rgBody.velocity, Vector3.zero, .4f);
-            rgBody.velocity = vel;
+            Vector3 vel = Vector3.Lerp(_rgBody.velocity, Vector3.zero, .4f);
+            _rgBody.velocity = vel;
         }
     }
 
 
     public void DoMovement(Vector3 direction, bool interactInput)
     {
-        animationControl.SetBool("isWalking", (direction != Vector3.zero) && canMove);
-        animationControl.SetBool("Grounded", isGrounded);
 
-        if (canMove)// movement handling
+        _animationControl.SetBool("isWalking", (direction != Vector3.zero) && _canMove);
+        _animationControl.SetBool("Grounded", _isGrounded);
+
+        GroundCheck();
+
+        if (_canMove)
         {
             direction *= moveForceModifier;
 
-            Vector3 velocity = rgBody.velocity - movingGroundVelocity;
+            Vector3 velocity = _rgBody.velocity - _movingGroundVelocity;
             Vector3 velocityChange = (direction - velocity);
 
             velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
             velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
             velocityChange.y = 0;
 
-            rgBody.AddForce(velocityChange, ForceMode.VelocityChange);
+            _rgBody.AddForce(velocityChange, ForceMode.VelocityChange);
 
             if (direction != Vector3.zero)
             {
@@ -86,87 +76,46 @@ public class HeroMove : MonoBehaviour {
 
         }
 
-        if (manualGravity)
-        {
-            // We apply gravity manually for more tuning control
-            rgBody.AddForce(new Vector3(0, -gravity * rgBody.mass, 0));
-        }
+        
+        // We apply gravity manually for more tuning control
+        _rgBody.AddForce(new Vector3(0, -gravity * _rgBody.mass, 0));
+        
 
         //Reset the moveDamp modifiers
-        movingGroundVelocity = Vector3.zero;
+        _movingGroundVelocity = Vector3.zero;
     }
 
-    private bool GroundCheck()
+    private void GroundCheck()
     {
-        if (capsCollider.enabled == true)
+        if (_capsCollider.enabled == true)
         {
-            Vector3 groundCheckPos = transform.TransformPoint(capsCollider.center);
-            groundCheckPos.y += groundedOffsetDistance;
+            Vector3 groundCheckPos = transform.TransformPoint(_capsCollider.center);
+            groundCheckPos.y += _groundedOffsetDistance;
 
-            Collider[] colliders = Physics.OverlapSphere(groundCheckPos, groundedRadius, whatIsGround);
-            for (int i = 0; i < colliders.Length; ++i)
+            Collider[] colliders = Physics.OverlapSphere(groundCheckPos, GroundedRadius, whatIsGround);
+            foreach (Collider collidingObject in colliders)
             {
-                if ((colliders[i].gameObject != gameObject) && !colliders[i].gameObject.name.Equals("_SweepTestObj_"))
+                if (collidingObject.gameObject != gameObject)
                 {
-                    //Check for moving ground
-                    if ((colliders[i].gameObject.layer == LayerMask.NameToLayer("MovingGround")) && (movingGroundVelocity == Vector3.zero))
-                    {
-                        movingGroundVelocity = colliders[i].gameObject.GetComponent<Rigidbody>().velocity;
-                    }
+                    HandleMovingGround(collidingObject.gameObject);
 
-                    Vector3 currentGroundBlockTopPos = colliders[i].transform.position;
-                    currentGroundBlockTopPos.y += colliders[i].bounds.center.y + colliders[i].bounds.extents.y;
-                    return true;
+                    _isGrounded = true;
                 }
             }
         }
-        //Check for moving ground
-        if (movingGroundVelocity != Vector3.zero)
-            movingGroundVelocity = Vector3.zero;
+        ////Check for moving ground
+        //if (_movingGroundVelocity != Vector3.zero)
+        //    _movingGroundVelocity = Vector3.zero;
 
-
-        return false;
+        _isGrounded = false;
     }
 
-    public Vector3 GetNearestAxis(Vector3 direction)
+    private void HandleMovingGround(GameObject movingGround)
     {
-        if (Mathf.Abs(direction.z) > Mathf.Abs(direction.x))
+        if ((movingGround.layer == LayerMask.NameToLayer(MOVING_GROUND_LAYER_NAME)) &&
+            (_movingGroundVelocity == Vector3.zero))
         {
-            if (direction.z > 0)
-                return Vector3.forward;
-            else
-                return -Vector3.forward;
-        }
-        else
-        {
-            if (direction.x > 0)
-                return Vector3.right;
-            else
-                return -Vector3.right;
-        }
-    }
-
-    float CalculateJumpVerticalSpeed()
-    {
-        // From the jump height and gravity we deduce the upwards moveDamp 
-        // for the character to reach at the apex.
-        // By using the Magic of Meth (yes meth)
-        return Mathf.Sqrt(2 * jumpHeight * gravity);
-    }
-
-    public bool SweepTestCheck(GameObject obj, Vector3 direction, float distance, out RaycastHit hitInfo)// returns true if something other than this object is hit
-    {
-        if (obj.transform.FindChild("_SweepTestObj_") == null)
-            throw new System.Exception("A Sweep Test Object needs to be attached to gameobject '" + obj.name + "' !!");
-
-        if (!obj.transform.FindChild("_SweepTestObj_").GetComponent<Rigidbody>().SweepTest(direction, out hitInfo, distance, QueryTriggerInteraction.Ignore))
-            return false;
-        else
-        {
-            if (hitInfo.collider.gameObject == obj)
-                return false;
-            else
-                return true;
+            _movingGroundVelocity = movingGround.gameObject.GetComponent<Rigidbody>().velocity;
         }
     }
 }
